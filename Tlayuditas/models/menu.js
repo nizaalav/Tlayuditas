2ede14f0f448c6b2f86c34d9b21a0bc8d990bf73
Darigo98 <trident_foshay@hotmail.com>
2021-10-15 00:31:34 -0500

    actualizando proyecto tlayuditas

--- Tlayuditas/models/menu.js
+++ Tlayuditas/models/menu.js
@@ -3,10 +3,7 @@ const Schema = mongoose.Schema
 
 const menuSchema = new Schema({
 
-  name: { type: String, default: 'Niza'},
-  lastName: { type: String, default: 'Alavat'},
-  phone: { type: String, default: '9712657084'},
-  genre: {type: String, enum: ['H', 'M', 'U'], default: 'U'},
+  category: {type: String, default:  'comida ' },
   email: {required: true, type: String, lowercase: true, trim: true, unique: true},
   location: {type: String, lowercase: true, trim: true, default: 'Coatzacoalcos, Ver.'},
   recommendation:{type: String, lowercase: true, trim: true, default: 'Muy Buenas, Sin Duda Volveria a Comerlas!'},
--- Tlayuditas/models/pedidos.js
+++ Tlayuditas/models/pedidos.js
@@ -3,10 +3,9 @@ const Schema = mongoose.Schema
 
 const pedidosSchema = new Schema({
 
-  name: { type: String, default: 'Niza'},
-  lastName: { type: String, default: 'Alavat'},
+  
   phone: { type: String, default: '9712657084'},
-  genre: {type: String, enum: ['H', 'M', 'U'], default: 'U'},
+  date:{ type: String},
   email: {required: true, type: String, lowercase: true, trim: true, unique: true},
   location: {type: String, lowercase: true, trim: true, default: 'Coatzacoalcos, Ver.'},
   recommendation:{type: String, lowercase: true, trim: true, default: 'Muy Buenas, Sin Duda Volveria a Comerlas!'},
